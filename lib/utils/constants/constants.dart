d1478fabace38eb1e4273bf4f6655019c58707ba
Hossam-Abdou <hossam.abdou.almasry@gmail.com>
2025-05-19 14:22:29 +0300

    edit

---
+++ lib/utils/constants/constants.dart
@@ -0,0 +1,26 @@
+class Constants {
+static const String apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4N2QyYjE4MDgyNzc3YmU2MDNlNzEyYzU1YjZhMjAxMiIsIm5iZiI6MTcyNDc2MjA3Mi4yODE0NTMsInN1YiI6IjY1MjFjYjdmYWI1ZTM0MDBlMWQzMDQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DJCFutEdvaPTqib7nLjdxDJJ5UfGfq9KTii6hkfCIb0';
+static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500/';
+static const int    accountID = 20540587;
+static const List<String> images = [
+  'assets/images/action.jpeg',
+  'assets/images/adv.jpeg',
+  'assets/images/anime.jpeg',
+  'assets/images/comedy.jpeg',
+  'assets/images/crime.jpg',
+  'assets/images/documentary.jpg',
+  'assets/images/drama.jpg',
+  'assets/images/family.jpg',
+  'assets/images/fantasy.jpg',
+  'assets/images/history.jpg',
+  'assets/images/horror2.jpg',
+  'assets/images/music.jpg',
+  'assets/images/mystery.jpg',
+  'assets/images/romance.jpg',
+  'assets/images/science2.jpg',
+  'assets/images/tv.jpg',
+  'assets/images/thiller.jpg',
+  'assets/images/war.jpg',
+  'assets/images/western.jpg',
+];
+}
